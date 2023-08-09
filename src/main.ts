0ac9c1c08c3544d9b022c3bf3befc7587e64f421
Max <maxswaine@gmail.com>
2023-08-09 11:37:38 +0100

    displays morse code underneath. stil no styling but very functional

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import "./style.scss";
 import { translateIntoMorse } from "./translate";
 
-const morseInput = document.querySelector(".morse__input") as HTMLElement;
+const morseInput = document.querySelector(".morse__input") as HTMLInputElement;
 const morseOutput = document.querySelector(".morse__output") as HTMLElement;
+
+if (!morseInput) {
+  throw new Error("Issue with input");
+}
+
+if (!morseOutput) {
+  throw new Error("Issue with output");
+}
+
+const displayMorseCode = (text: string) => {
+  const morseArray = translateIntoMorse(text);
+  const morseText = morseArray.join(" ");
+  morseOutput.innerHTML = "";
+  const template = `<p>${morseText}</p>`;
+  morseOutput.innerHTML = template;
+};
+
+morseInput.addEventListener("input", (event: Event) => {
+  const text = (event.target as HTMLInputElement).value;
+  displayMorseCode(text);
+});
